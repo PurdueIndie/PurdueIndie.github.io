71e3adc376a807170301119962a1e9775eed01e5
Charlie Su <Charlie Su>
2016-01-24 16:16:27 -0500

    added comments to parallelax code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).scroll(function()
 {
-		var scroll = $(document).scrollTop();
-	    var emSize = parseFloat($("body").css("font-size"));
-		$("#title").css("top", (10*emSize-scroll*0.4)+"px");
+		var scroll = $(document).scrollTop();//gets how much we have scrolled
+	    var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
+		$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
+		//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
 });
