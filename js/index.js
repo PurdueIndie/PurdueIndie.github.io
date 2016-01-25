1dd15dbc958717223d34d4297d46a2c4078e755c
Charlie Su <Charlie Su>
2016-01-24 20:14:59 -0500

    Implemented resize functionality

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
+$(document).ready(function()
+{
+	horizontallyCenter("#title");//okay line
+});
+
 $(document).scroll(function()
 {
-		var scroll = $(document).scrollTop();//gets how much we have scrolled
-	    var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
-		$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
-		//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
+	var scroll = $(document).scrollTop();//gets how much we have scrolled
+    var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
+	$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
+	//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
+});
+
+$(window).resize(function()
+{
+	horizontallyCenter("#title");
 });
 
 function horizontallyCenter(thingYouWantToCenter)
 {
 	console.log("centered");
-	$(thingYouWantToCenter).css("left", (($(window).width() - $(thingYouWantToCenter).css("width"))/2) + "px");
-	console.log("not centered");
+	var pos = (($(window).width() - parseFloat($(thingYouWantToCenter).css("width")))/2);
+	console.log(pos);
+	$(thingYouWantToCenter).css("left", pos + "px");
 }
-//play with it on your browser
-horizontallyCenter("#title");//okay line
