cd54335a4848fdc36de7c8dc9265cfcedca96873
Charlie Su <Charlie Su>
2016-01-24 21:04:36 -0500

    Resized title. Added more spacing between the title's lines. Made the parallex effect its own func

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 $(document).ready(function()
 {
 	horizontallyCenter("#title");//okay line
+	parallax();
 });
 
-$(document).scroll(function()
-{
-	var scroll = $(document).scrollTop();//gets how much we have scrolled
-    var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
-	$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
-	//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
-});
+$(document).scroll(parallax);
 
 $(window).resize(function()
 {
@@ -22,4 +17,11 @@ function horizontallyCenter(thingYouWantToCenter)
 	var pos = (($(window).width() - parseFloat($(thingYouWantToCenter).css("width")))/2);
 	console.log(pos);
 	$(thingYouWantToCenter).css("left", pos + "px");
+}
+function parallax()
+{
+	var scroll = $(document).scrollTop();//gets how much we have scrolled
+    var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
+	$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
+	//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
 }
