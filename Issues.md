e90898083c8f839a74ec383512c0b1a5b405c849
Charlie Su <dragonsmirth@gmail.com>
2016-08-04 21:42:31 +0800

    Got the textshadow to change position with scrolling somehow

---
+++ Issues.md
@@ -0,0 +1,2 @@
+1. The right invisible menu is clickable
+2. The website is 100% not mobile friendly
--- js/index.js
+++ js/index.js
@@ -23,7 +23,8 @@ function parallax()
 	var scroll = $(document).scrollTop();//gets how much we have scrolled
     var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
 	$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
-	console.log("7px 7px 3px rgba(255, 204, 38, " + (scroll*0.01) + ");");
-	$("#title").css("textShadow", "7px 7px 3px rgba(255, 204, 38, " + (scroll*0.01) + ");");
+	var txt_shadow = 7 - (scroll * 0.05);
+	//$("#title").css("textShadow", text_shadow_css);
+	$("#title").css("textShadow", txt_shadow  + "px " + txt_shadow + "px 3px rgba(255, 204, 38, 1)");
 	//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
 }
