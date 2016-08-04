463991e1aa65be500d93b4b1b2ed2973b6d46da2
Charlie Su <dragonsmirth@gmail.com>
2016-08-04 20:44:49 +0800

    Changed font of title to 8bit; added nice looking text shadow; changing color of buttons

--- js/index.js
+++ js/index.js
@@ -23,5 +23,7 @@ function parallax()
 	var scroll = $(document).scrollTop();//gets how much we have scrolled
     var emSize = parseFloat($("body").css("font-size"));//convert em (what we used in css) to px
 	$("#title").css("top", (10*emSize-scroll*0.4)+"px");//scale down the scroll factor by 0.4
+	console.log("7px 7px 3px rgba(255, 204, 38, " + (scroll*0.01) + ");");
+	$("#title").css("textShadow", "7px 7px 3px rgba(255, 204, 38, " + (scroll*0.01) + ");");
 	//10 was our original emSize in css. So it's 10em - how much we scrolled*slow factor = slower scrolling!
 }
